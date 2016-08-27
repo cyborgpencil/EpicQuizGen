e0cce850e777cb7c20b73e28c09ed8f27dc7af76
cyborgpencil <cyborgpencil@gmail.com>
2016-08-27 02:16:56 -0600

    Created Dispatcher Timer Class

---
+++ EpicQuizGen/Utils/QuizTimeManager.cs
@@ -0,0 +1,67 @@
+﻿using EpicQuizGen.ViewModels;
+using Prism.Mvvm;
+using System;
+using System.Diagnostics;
+using System.Windows.Threading;
+
+namespace EpicQuizGen.Utils
+{
+    public class QuizTimeManager : BindableBase
+    {
+        private DispatcherTimer DispatcherTimer { get; set; }
+        public int QuizTimerIntevalHours { get; set; }
+        public int QuizTimerIntervalMinutes { get; set; }
+        public int QuizTimeIntervalSeconds { get; set; }
+        private int _quizTimerCounter;
+
+        private int _quizTimerCountet;
+        public int QuizTimerCounter
+        {
+            get { return _quizTimerCountet; }
+            set { SetProperty(ref _quizTimerCountet, value); _quizTimerCounter = value;
+                if (value < 0)
+                {
+                    DispatcherTimer.Stop();
+                }
+            }
+        }
+        
+        private QuizTakeViewModel _quizTakeViewModel;
+
+        private delegate void Display();
+
+        public QuizTimeManager(QuizTakeViewModel quizTakeViewModel)
+        {
+            _quizTakeViewModel = quizTakeViewModel;
+            // each quiz will be at lease 5 secs
+            QuizTimeIntervalSeconds = 1;
+            QuizTimerCounter = 5;
+            DispatcherTimer = new DispatcherTimer();
+            DispatcherTimer.Tick += _quizTakeViewModel.QuizTimerEvent;
+            DispatcherTimer.Interval = new TimeSpan(QuizTimerIntevalHours, QuizTimerIntervalMinutes, QuizTimeIntervalSeconds);
+        }
+
+        public void StartTimer()
+        {
+            DispatcherTimer.Start();
+        }
+
+        public void StopTimer()
+        {
+            DispatcherTimer.Stop();
+        }
+
+        public int SendCurrentSecound()
+        {
+            int dec = QuizTimerCounter;
+            QuizTimerCounter -= 1;
+            return dec;
+        }
+
+        public bool GetTimerEnabled()
+        {
+            return DispatcherTimer.IsEnabled;
+
+        }
+    }
+}
--- EpicQuizGen/ViewModels/QuizTakeViewModel.cs
+++ EpicQuizGen/ViewModels/QuizTakeViewModel.cs
@@ -5,13 +5,14 @@ using Prism.Mvvm;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EpicQuizGen.ViewModels
 {
-    class QuizTakeViewModel : BindableBase
+    public class QuizTakeViewModel : BindableBase
     {
         #region Properties
         private string _timeLeftString  ;
@@ -44,6 +45,13 @@ namespace EpicQuizGen.ViewModels
             get { return _currentQuestionType; }
             set { SetProperty(ref _currentQuestionType, value); }
         }
+
+        private QuizTimeManager _quizTimer;
+        public QuizTimeManager QuizTimer
+        {
+            get { return _quizTimer; }
+            set { SetProperty(ref _quizTimer, value); }
+        }
         private IRegionManager _regionManager { get; set; }
         #endregion
 
@@ -52,11 +60,13 @@ namespace EpicQuizGen.ViewModels
         {
             _regionManager = regionManager;
             Quiz = QuizIOManager.Instance.Quiz;
-            Timer = 1;
+
             TimeLeftString = "";
             CurrentQuestion = Quiz.Questions[0];
             CurrentQuestionType = CurrentQuestion.QuestionType;
             NavigateCommand = new DelegateCommand<string>(Navigate);
+
+            
         }
         #endregion
 
@@ -64,11 +74,19 @@ namespace EpicQuizGen.ViewModels
         public DelegateCommand<string> NavigateCommand { get; set; }
         public void Navigate(string uri)
         {
+            // Timer
+            QuizTimer = new QuizTimeManager(this);
+            QuizTimer.StartTimer();
+            Timer = QuizTimer.SendCurrentSecound();
             _regionManager.RequestNavigate("CurrentQuestionType", uri);
         }
         #endregion
 
         #region Events
+        internal void QuizTimerEvent(object sender, EventArgs e)
+        {
+            Timer = QuizTimer.SendCurrentSecound();
+        }
         #endregion
 
         #region Methods
