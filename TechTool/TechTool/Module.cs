311d865ddb19f09e202fcd4fef22bec98a2ed645
Erik Morrow <cyborgpencil@gmail.com>
2017-03-22 12:39:46 -0600

    Inital Project Created

---
+++ TechTool/TechTool/Module.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Modularity;
+using Prism.Regions;
+using System;
+
+namespace TechTool
+{
+    public class TechToolModule : IModule
+    {
+        IRegionManager _regionManager;
+
+        public TechToolModule(RegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
